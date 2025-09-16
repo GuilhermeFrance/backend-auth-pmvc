39812d960fd0e65603605ffa7fc83ec514ac296a
Guilherme <guilhermefrance9@gmail.com>
2025-09-16 16:51:12 -0300

    generate token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guards';
+import { AuthRequest } from './models/AuthRequest';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+@Post('login')
+@HttpCode(HttpStatus.OK)
+@UseGuards(LocalAuthGuard)
+login(@Request() req) {
+    console.log(req.user)
+   
+    return this. authService.login(req.user);
+    
+}}
---
+++ src/auth/models/UserToken.ts
@@ -0,0 +1,5 @@
+
+
+export interface UserToken {
+    acess_token: string;
+}
